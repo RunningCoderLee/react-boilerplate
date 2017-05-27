6936a4f5cdc9765047ea2b6c61af3084b26471c3
KevinLee <lixin@romenscd.cn>
2017-05-27 18:29:34 +0800

    update create react app to 1.0.6

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Logo(props) {
+
+  function handleClick() {
+    props.onClick();
+  }
+
+  return (
+    <img
+      onClick={handleClick}
+      src={logo}
+      className="App-logo"
+      alt="logo" />
+  );
+}
+
 class App extends Component {
+  handleClick() {
+    this.setState({
+      hasClicked: true,
+    });
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Logo onCLick={this.handleClick} />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
